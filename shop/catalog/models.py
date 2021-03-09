e8fbacedb0568dcc1e1b346ad38df44d0472adce
Hood <hoodkhizer@gmail.com>
2021-03-09 16:07:40 +0500

    Models migrated

--- shop/catalog/models.py
+++ shop/catalog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class LU_Status(models.Model):
+    status_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=300)
+    status = models.ForeignKey(LU_Status, on_delete=models.DO_NOTHING)
+
